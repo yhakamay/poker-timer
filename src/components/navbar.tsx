07c2e25695a2a4dfab9835e3b91a72c179fdccb0
Yusuke Hakamaya <yhakamaya1031@gmail.com>
2024-12-07 13:16:18 +0900

    add Navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div className="navbar absolute top-0 flex flex-row justify-center md:justify-start pl-8">
+      <h1 className="text-2xl font-mono font-bold">Poker Timer</h1>
+    </div>
+  );
+}
